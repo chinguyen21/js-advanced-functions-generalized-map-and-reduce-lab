cd1f9e1769c69003baae8c6039abf45f7b72a287
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-01-15 16:32:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+
+const map = (arr, func) => arr.map(ele => func(ele))
+
+const reduce = (arr, func, startingPoint) => {
+  if (startingPoint) {
+  return arr.reduce((total, ele) => func(total, ele),startingPoint)
+  } else {
+    if (arr.every(ele => typeof ele === "number")) {
+      return arr.reduce((total, ele) => func(total, ele),0)
+    } else {
+      return arr.reduce((total, ele) => func(total, ele), true)
+    }
+  }
+
+}
