8bbf9adbd5aa2d4744d703c888791436e0a56a99
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-01-15 20:56:59 -0500

    commit tree 76b88a3daa40fe3cf17c5a3727ad4b79e91d9434

--- index.js
+++ index.js
@@ -3,14 +3,23 @@
 const map = (arr, func) => arr.map(ele => func(ele))
 
 const reduce = (arr, func, startingPoint) => {
-  let r = startingPoint
-  if (!startingPoint) {
-    if (arr.every(ele => typeof ele === "number")) {
-      r = 0
-    } else {
-      r = true
-    }
-  }        
-  return arr.reduce((total, ele) => func(total, ele),r)
-          
+  if (startingPoint) {
+    return arr.reduce((total, ele) => func(total, ele),startingPoint)
+  } else {
+    return arr.reduce((total, ele) => func(total, ele))
+  }     
 }
+
+
+// const reduce = (arr, func, startingPoint) => {
+//   let r = startingPoint
+//   if (!startingPoint) {
+//     if (arr.every(ele => typeof ele === "number")) {
+//       r = 0
+//     } else {
+//       r = true
+//     }
+//   }        
+//   return arr.reduce((total, ele) => func(total, ele),r)
+          
+// }
